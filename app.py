cfab576718a124f217013425122cae7c5df807ad
Satyendra Singh <satyendrasinghdpu@gmail.com>
2026-05-03 17:45:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/14r6_CbEl_Pa3Z1saiiQwgpaVukvbagFi
 """
 
-!pip install streamlit
 
 import streamlit as st
 import numpy as np
